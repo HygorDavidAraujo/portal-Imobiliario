1f0dea241a30cc901a4cbcbff8d3d136143872bb
Hygor Araujo <hygordavidaraujo@gmail.com>
2025-12-23 00:52:05 -0300

    correção de import nao usado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ImoveisProvider } from './contexts/ImoveisContext';
 import { Catalogo } from './pages/Catalogo';
